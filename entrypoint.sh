b41287a99eb90078e1fc675d05a0df03e7df5c74
Jean-Philippe Schneider <jeanpylone@users.noreply.github.com>
2024-04-08 13:45:50 +0200

    Update entrypoint.sh
    make run option optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ fi
 if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
 fi
-
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+if [ ! -z $INPUT_RUN ];
+then exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+else
+exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE
+fi
